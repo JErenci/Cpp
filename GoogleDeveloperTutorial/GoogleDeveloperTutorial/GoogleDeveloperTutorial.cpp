57119cdff83f572214bbb217e66d439cd0600d9a
Juan Erencia Guerrero <juan.erencia.guerrero@gmail.com>
2015-11-26 23:53:55 +0100

    Hello World test in C++

---
+++ GoogleDeveloperTutorial/GoogleDeveloperTutorial/GoogleDeveloperTutorial.cpp
@@ -0,0 +1,16 @@
+// GoogleDeveloperTutorial.cpp : Defines the entry point for the console application.
+// Juan Erencia Guerrero (Juan.erencia.guerrero@gmail.com)
+// Description: a program that prints the immortal saying "hello world"
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	int x;
+	cin >> x;
+	return 0;
+}
+
+
